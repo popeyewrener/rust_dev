b1c4db966a02403bb958268afee689aafbdc9640
popeyewrener <popwren@gmail.com>
2025-05-30 18:23:48 +0530

    simple rust hello world

---
+++ init_rust/main.rs
@@ -0,0 +1,6 @@
+fn main(){
+    println!("Hello, world!");
+    // This is a simple Rust program that prints "Hello, world!" to the console.
+    //compile with: rustc main.rs
+    // To run the program, use the command: ./main
+}
