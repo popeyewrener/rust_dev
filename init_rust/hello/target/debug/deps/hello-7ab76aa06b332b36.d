7b1edc3ab6db84a38fb9612dffab8aee4b05c6e9
popeyewrener <popwren@gmail.com>
2025-05-30 18:26:05 +0530

    simple rust hello world with cargo

---
+++ init_rust/hello/target/debug/deps/hello-7ab76aa06b332b36.d
@@ -0,0 +1,5 @@
+/Users/chaitanyakanhar/Developer/rust_dev/init_rust/hello/target/debug/deps/hello-7ab76aa06b332b36: src/main.rs
+
+/Users/chaitanyakanhar/Developer/rust_dev/init_rust/hello/target/debug/deps/hello-7ab76aa06b332b36.d: src/main.rs
+
+src/main.rs:
---
+++ init_rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/chaitanyakanhar/Developer/rust_dev/init_rust/hello/target/debug/hello: /Users/chaitanyakanhar/Developer/rust_dev/init_rust/hello/src/main.rs
